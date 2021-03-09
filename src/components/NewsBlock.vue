fe9a997be5bb8c77fb12dc3885a8c26a3f300212
Raku <raul.r.h.rosas@gmail.com>
2021-03-08 16:39:37 -0800

    Multiple charts now working

---
+++ src/components/NewsBlock.vue
@@ -0,0 +1,23 @@
+<template>
+    <div style="width:100%; height:100%;">
+        <a :href="news.url">
+        <h3>{{news.headline}}</h3>
+        <img :src="news.image" class="icon">
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'NewsBlock',
+    props: ['news']
+}
+</script>
+
+<style scoped>
+.icon{
+    width:10%;
+    height:10%;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,128 @@
 <template>
   <div>
     <h1>Your portfolio</h1>
-    <div v-if="getPortfolio.length == 0">
-      You have no stocks in your portfolio yet!
-    </div>
-    <div v-if="getPortfolio.length > 0" class="stock-container">
+    <div v-if="getPortfolio.length > 0">
       <StockItem v-for="stock in getPortfolio" :key="stock.symbol" v-bind:stock ="stock"/>
+      <h1>Here's how your stock has performed lately</h1>
+      <line-chart v-if="loaded" :chart-data= 'line' :options="options"/>
+      <h1>The latest talk on your portfolio</h1>
+      <NewsBlock v-for="(piece, index) in filtered" :key="index" v-bind:news="piece"/>
     </div>
-    <h1>Here's how your stock has performed lately</h1>
-    <h1>The latest talk on your portfolio</h1>
+    <p v-if="getPortfolio.length == 0">You have no stocks in your portfolio yet!</p>
   </div> 
 </template>
 
 <script>
 import {mapGetters, mapActions} from 'vuex'
 import StockItem from '../components/StockItem'
+import NewsBlock from '../components/NewsBlock'
+import LineChart from '../components/LineChart'
+import axios from 'axios'
 export default {
   name: 'Home',
   computed: mapGetters(['getPortfolio']),
+  data(){
+    return {
+      news: [],
+      filtered: [],
+      line: null,
+      loaded: false,
+      dates:'',
+      options: {
+        scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero: true
+              }
+          }]
+        }
+      }
+    }
+  },
   methods: {
-    ...mapActions(['quotePortfolio'])
+    ...mapActions(['quotePortfolio']),
+    dateToString(date){
+        //This is how finnhub styles their dates
+        let day = String(date.getDate()).padStart(2,'0')
+        let month = String(date.getMonth() + 1).padStart(2, '0')
+        return date.getFullYear() + '-' + month + '-' + day
+    },
+    getDates(){
+      let date = new Date()
+      let today = this.dateToString(date)
+      let m_date = new Date()
+      m_date.setDate(m_date.getDate() - 7)
+      let week_ago = this.dateToString(m_date) 
+      let today_unix = date.getTime()
+      let week_ago_unix = m_date.getTime()
+      return {today, week_ago, today_unix, week_ago_unix}
+    },
+    getFiltered(){
+      this.news.forEach(
+        stock=>{
+          stock.data.slice(0, 5).forEach(
+            data=>this.filtered = [...this.filtered, data]
+          )
+        }
+      )
+    },
+    getNews(){
+      let promises = []
+      this.getPortfolio.forEach(stock => {
+        promises.push(axios.get('https://finnhub.io/api/v1/company-news?symbol='+stock.symbol+'&from='+this.dates.week_ago+'&to='+this.dates.today+'&token='+process.env.VUE_APP_FINNHUB))
+      });
+      Promise.all(promises).then(
+        res=> {
+          this.news = res
+        } 
+      ).then(
+          () => this.getFiltered()
+      )
+    },
+    getStockCandles(){
+      let promises = []
+      this.getPortfolio.forEach(stock => {
+        promises.push(axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol='+stock.symbol+'&apikey='+process.env.VUE_APP_ALPHA).then(res => {return {res, symbol:stock.symbol}}))
+      });
+      this.line = {
+        labels:[],
+        datasets:[]
+      }
+      Promise.all(promises).then(
+        res=> {
+          let lbls = res[0].res.data['Time Series (Daily)']
+          Object.keys(lbls).forEach(date=>{
+            this.line.labels.push(date)
+          })
+          res.forEach(stock=>{
+            let subset = {
+              label:stock.symbol,
+              backgroundColor: '#' + Math.floor(Math.random()*16777215).toString(16),
+              fillOpacity: .3, 
+              data: []
+            } 
+            let data = stock.res.data['Time Series (Daily)']
+            Object.keys(data).forEach(date=>{
+              subset.data.push(data[date]['4. close'])
+            })
+            this.line.datasets.push(subset)
+          })
+        } 
+      ).then(
+        ()=> this.loaded = true
+      )
+    }
   },
   components: {
-    StockItem
+    StockItem,
+    NewsBlock,
+    LineChart
   },
   created(){
     this.quotePortfolio()
+    this.dates = this.getDates()
+    this.getNews()
+    if(this.getPortfolio.length !==0) this.getStockCandles()
   }
 }
 </script>
@@ -36,6 +133,5 @@ export default {
   flex-direction: colum;
   flex-wrap: wrap;
   justify-content: space-evenly;
-  width: 100%;
 }
 </style>
