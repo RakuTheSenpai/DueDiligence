f66a7e0fc69506603842d2b9186de09d3dcd9c8b
Raku <raul.r.h.rosas@gmail.com>
2021-03-21 11:34:14 -0700

    Prettify company page

--- src/views/Company.vue
+++ src/views/Company.vue
@@ -1,11 +1,19 @@
 <template>
-    <div>
-       <div style="display:inline-block">
-            <a :href="company_design.weburl"><img :src="this.company_design.logo !== ''?this.company_design.logo:require('@/assets/logo.png')"></a>
-            <h1>{{company_design.name}} | {{company_design.ticker}} </h1>
-       </div>
-       <h3>Industry: {{company_design.finnhubIndustry}}</h3>
-        <p> {{company_info.Description}}</p>
+    <div class="container">
+        <div class="header">
+            <a :href="company_design.weburl" class="img-link"><img :src="this.company_design.logo !== ''?this.company_design.logo:require('@/assets/company.png')"></a>
+            <div class="company-header">
+                <h1>{{company_design.name}} | {{company_design.ticker}} </h1>
+                <h3>Industry: {{company_design.finnhubIndustry}}</h3>
+            </div>
+        </div>
+        <p class="description">{{description}}</p>
+        <div class="table">
+            <div class="table-entry" v-for="(key, index) in company_info" :key="index">
+                <h3> {{index}} </h3>
+                <h3> {{key}} </h3>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -17,7 +25,8 @@ export default {
     data(){
         return {
             company_design:{},
-            company_info: ''
+            company_info: {},
+            description: ''
         }
     },
     methods:{
@@ -25,13 +34,19 @@ export default {
            axios.get('https://finnhub.io/api/v1/stock/profile2?symbol='+ this.id +'&token=' + process.env.VUE_APP_FINNHUB).then(
                 res => {
                     this.company_design = res.data
+                    console.log(res)
                 }
            )
         },
         fetchCompanyInfo(){
             axios.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol='+this.id+'&apikey=' + process.env.VUE_APP_ALPHA).then(
                 res => {
+                    this.description = res.data.Description 
                     this.company_info = res.data
+                    delete this.company_info.Description
+                    delete this.company_info.Name 
+                    delete this.company_info.Industry
+                    delete this.company_info.Symbol
                 }
             )
         }
@@ -43,6 +58,40 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.header{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    border-bottom: 1px white solid;
+}
+.img-link{
+    flex-basis: 20%;
+}
+.company-header{
+    display: flex;
+    flex-direction: column;
+    flex-basis: 80%;
+}
+.container{
+    margin-left: 10%;
+    margin-right: 10%;
+}
+.description{
+    text-align: justify;
+    border-bottom: 1px white solid;
+    padding: 1%;
+}
+.table{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+.table-entry{
+    flex-basis: 48%;
+    background-color: rgba(0, 0, 0, 0.199);
+    margin: 1%;
+    display: flex;
+    flex-direction: column;
+}
 </style>
